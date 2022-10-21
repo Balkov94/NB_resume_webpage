39e83a56fd3dd8e0ea2190269cda1436f8a4a36a
N.Balkov <balkov_94@abv.bg>
2022-10-21 17:22:54 +0300

    Added few new technologies fiexd mobile styles

--- js/personal_project_video_switching.js
+++ js/personal_project_video_switching.js
@@ -3,12 +3,13 @@
      let acordeonButtons = document.getElementsByClassName("btn btn-link");
 
      // all videos src
+     let GoodBook = "assets/projects_video_files/goodBook.mp4";
      let glovo = "assets/projects_video_files/glVideo.mp4";
      let cinema = "assets/projects_video_files/movies_fetching_project_video.mp4";
      let recipes = "assets/projects_video_files/cooking_webpage_video.mp4";
      let game2048 = "assets/projects_video_files/2048_game_video.mp4";
      let simpleBusiness = "assets/projects_video_files/friend_project_video.mp4";
-     let videoSrc = [glovo, cinema, recipes, game2048, simpleBusiness];
+     let videoSrc = [GoodBook, glovo, cinema, recipes, game2048, simpleBusiness];
 
      for (let i = 0; i < acordeonButtons.length; i++) {
           acordeonButtons[i].addEventListener("click", function () {
